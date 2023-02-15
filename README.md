7228db10bc716c21c0f7466d4aa3ca4c352872bb
sameerv-cemtrex <svanjari@cemtrexlabs.com>
2023-02-15 15:58:29 +0530

    Cockatoo - extension added

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
+# Cockatoo
+
+This is a chrome extension that helps you quench your thirsts by suggesting you exciting cocktails on every click. You can also try them on your own following the recipes giving to you along with it. 
+
+Try now.
+
+
+![Screenshot](./ss.png)
+
+<br>
+<br>
+
+## Direct use
+
+You can directly use the extension after cloning the repo and following the steps mentioned [here](https://www.freecodecamp.org/news/building-chrome-extension/) in the **Manifest.json** section. You can also create your own extension if you follow the steps written in there.
+<br><br><br>
+
+## next-chrome
+
 `next-chrome` is a [Next.js](https://nextjs.org/) starter project to bootstrap a new Chrome extension.
 
 [Helpful Tips for Starting a Next.js Chrome Extension | CSS-Tricks](https://css-tricks.com/nextjs-chrome-extension-starter/)
--- next-app/pages/index.js
+++ next-app/pages/index.js
@@ -1,66 +1,72 @@
 import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
 
 const IndexPage = () => {
+  const [cocktail, setCocktail] = useState(null);
+  const [showRecipe, setShowRecipe] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const fetchData = async () => {
+    const resp = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
+    const res = await resp.json();
+    setCocktail(res.drinks[0]);
+    setLoading(false);
+  };
+  const drinkList = [];
+
+  if (!isLoading) {
+    for (let x = 1; x <= 15; x++) {
+      let drinkNo = "strIngredient";
+      if (cocktail[drinkNo + x] !== null) {
+        drinkList.push(cocktail[drinkNo + x]);
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      {isLoading ? (
+        <p style={{ color: "white" }}>Loading....</p>
+      ) : (
+        <>
+          <h1 className={styles.title}>{cocktail.strDrink}</h1>
+          <h4 className={styles.subtitle}>{cocktail.strCategory}</h4>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <img
+            src={cocktail.strDrinkThumb}
+            alt="drink"
+            width={300}
+            height={300}
+            className={styles.img}
+          />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <h3>Ingredients:</h3>
+          <ul className={styles.list}>
+            {drinkList.map((item) => (
+              <li className={styles.listItem} key={item}>
+                {item}
+              </li>
+            ))}
+          </ul>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <img
-              src="assets/vercel.svg"
-              alt="Vercel Logo"
-              width={72}
-              height={16}
-            />
-          </span>
-        </a>
-      </footer>
+          <div>
+            <h3
+              onClick={() => setShowRecipe(!showRecipe)}
+              className={styles.recipeBtn}
+            >
+              Recipe
+            </h3>
+            {showRecipe ? (
+              <div className={styles.recipe}>{cocktail.strInstructions}</div>
+            ) : null}
+          </div>
+        </>
+      )}
     </div>
   );
 };
